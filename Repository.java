bea5787f5685bb1bec41561d12030e592157976d
eric <eberber01@gmail.com>
2024-02-20 16:01:46 -0800

    Create Repository

---
+++ Repository.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.awt.*;
+import java.beans.PropertyChangeSupport;
+public class Repository extends PropertyChangeSupport {
+    private static Repository single_instance = null;
+ 
+    private ArrayList<Shape> shapes;
+
+    private Repository(){
+        super(new Object());
+        shapes = new ArrayList<Shape>();
+    }
+
+    public void addShape(Shape shape){
+        shapes.add(shape);
+        firePropertyChange("New Shape", null, shape);
+    }
+ 
+    public static synchronized Repository getInstance(){
+        if (single_instance == null)
+            single_instance = new Repository();
+ 
+        return single_instance;
+    } 
+}
